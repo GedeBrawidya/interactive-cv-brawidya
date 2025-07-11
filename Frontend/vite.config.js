2955efe49a9e178768bc32f8471a03fb6ee61d06
gedebrawidya <gedepujaa9@gmail.com>
2025-07-11 15:18:56 +0700

    deploy ilang

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/', // 🔴 Penting untuk deploy!
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  build: {
+    outDir: 'dist', // 🔴 Pastikan sesuai
+    emptyOutDir: true
+  }
 })
