b26e5c620a5409e5e6fd9fd68866f4081151b928
gedebrawidya <gedepujaa9@gmail.com>
2025-07-11 15:07:09 +0700

    deploy vercel

---
+++ Frontend/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Konten HTML Anda di sini -->
+  <div>
+    <h1>Tentang Saya</h1>
+    <!-- Isi konten tentang -->
+  </div>
+</template>
+
+<script setup>
+// Logika JavaScript di sini (gunakan Composition API)
+</script>
+
+<style scoped>
+/* CSS styling jika diperlukan */
+</style>
